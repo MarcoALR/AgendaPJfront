42ecbf645850ce33bafb50ff33cb55458332f1d4
Marco A. <161896294+MarcoALR@users.noreply.github.com>
2025-06-16 12:08:53 -0300

    🎈

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import Home from './pages/Home';
+import Criarcontato from './criarcontato.html';
 import Cadastrar from './pages/Home/cadastrar';
 
 createRoot(document.getElementById('root')).render(
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cadastrar" element={<Cadastrar />} />
+        <Route path="/criarcontato" element={<Criarcontato />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
