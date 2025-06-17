1ae33a34a474070eaf6fa237394cf9e6e1e10b01
Marco A. <161896294+MarcoALR@users.noreply.github.com>
2025-06-17 10:37:59 -0300

    🎈

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,27 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './index.css';
-import Home from './pages/Home';
-import Cadastrar from './pages/Home/cadastrar';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import Home from "./pages/Home";
+import Cadastrar from "./pages/Home/cadastrar";
+import CriarContato from "./pages/CriarContato"; 
+import PrivateRoute from "./components/PrivateRoute";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cadastrar" element={<Cadastrar />} />
+
+        <Route
+          path="/criarcontato"
+          element={
+            <PrivateRoute>
+              <CriarContato />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   </StrictMode>
