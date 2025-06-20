b7c120a8c25d5d155e59bac25535da74aec7a41d
Marco A. <161896294+MarcoALR@users.noreply.github.com>
2025-06-20 12:21:36 -0300

    🎈

--- src/pages/Favoritos.jsx
+++ src/pages/Favoritos.jsx
@@ -1,7 +1,7 @@
 // Favoritos.jsx
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./styles/favoritos.css";
+import "./../styles/favoritos.css";
 import logoImg from "./assets/agendapjlogo.png";
 
 function Favoritos() {
