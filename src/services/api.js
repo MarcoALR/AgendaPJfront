29cb6829a1dda10c3d37aa816d32e8b587a60cc2
Marco A. <161896294+MarcoALR@users.noreply.github.com>
2026-02-01 21:15:04 -0300

    🎈

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://apiusuariospj.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL || "https://apiusuariospj.onrender.com/",
 });
 
 api.interceptors.response.use(
