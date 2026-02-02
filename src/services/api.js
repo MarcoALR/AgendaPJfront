4a886a0c972a8a43fba4340366a50f4adfb7627c
Marco A. <161896294+MarcoALR@users.noreply.github.com>
2026-02-01 21:29:33 -0300

    🎈

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,21 @@
-// services/api.js
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://apiusuariospj.onrender.com/",
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-api.interceptors.response.use(
-  response => response,
-  async error => {
-    ...
+// Interceptor para adicionar o token automaticamente
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("accessToken");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
   }
-);
+
+  return config;
+});
 
 export default api;
